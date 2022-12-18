bca4399fdcab669a626d7a850f8f46ee1afd7078
Raul Correa <raulcorrea0110@hotmail.com>
2022-12-18 00:21:52 -0500

    Init workspace

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  // Se utiliza propiedades de padre
+  // <App saludo="Hello World!!"/>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // Se utiliza un hijo dentro de elemento
+  // <App>
+  //   <h1>HELLO WORLD CHILDREN</h1>
+  // </App>
+
+  <App />
+);
