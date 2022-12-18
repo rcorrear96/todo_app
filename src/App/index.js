949eed2e98d6bb880349e4fe4b8a5cefc4834abb
Raul Correa <raulcorrea0110@hotmail.com>
2022-12-18 17:34:36 -0500

    add new files of index.js

---
+++ src/App/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { AppUI } from './AppUI';
+
+const defaultTodos = [
+  {text: 'Cortar cebolla', completed: true},
+  {text: 'Tomar curso intro a React', completed: false},
+  {text: 'Llorar con React', completed: false},
+  {text: 'LALALAAL', completed: false}
+];
+
+function App() {
+  const [todos, setTodos] =  React.useState(defaultTodos);
+  const [searchValue, setSearchValue] =  React.useState('');
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  if(!searchValue.length>=1){
+    searchedTodos = todos;
+  } else{
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    })
+  }
+
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text ==text);
+    const newTodos = [...todos];
+    newTodos[todoIndex] = {
+      ...todos[todoIndex],
+      completed: true
+    };
+    setTodos(newTodos);
+  }
+
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text ==text);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  }
+
+  return (
+    <AppUI
+    total={totalTodos}
+    completed={completedTodos}
+    searchValue={searchValue}
+    setSearchValue={setSearchValue}
+    searchedTodos={searchedTodos}
+    completeTodo={completeTodo}
+    deleteTodo={deleteTodo}
+
+
+    />
+  )
+}
+
+export default App;
