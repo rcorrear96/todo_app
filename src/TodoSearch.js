59257c67ef608984897123e05f5faba4d13c067f
Raul Correa <raulcorrea0110@hotmail.com>
2022-12-18 09:33:38 -0500

    using state on element

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,17 +2,23 @@ import React from 'react';
 import './TodoSearch.css'
 
 function TodoSearch() {
+    const [searchValue, setSearchValue] =  React.useState('');
+
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value)
     }
-    return(
+
+    return[
         <input
             className='TodoSearch'
             placeholder="cebolla"
+            value={searchValue}
             onChange={onSearchValueChange}
         >
-        </input>
-    )
+        </input>,
+        <p>{searchValue}</p>
+    ];
 }
 
 export {TodoSearch}
