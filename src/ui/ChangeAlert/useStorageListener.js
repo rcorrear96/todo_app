7bf7982c93e5420abcb3d9cbf270eb41717f7d9a
Raul Correa <raulcorrea0110@hotmail.com>
2022-12-26 17:49:18 -0500

    adding ids to TodoList

--- src/ui/ChangeAlert/useStorageListener.js
+++ src/ui/ChangeAlert/useStorageListener.js
@@ -4,7 +4,7 @@ function useStorageListener(sincronize) {
   const [storageChange, setStorageChange] = React.useState(false);
   
   window.addEventListener('storage', (change) => {
-    if (change.key === 'TODOS_V1') {
+    if (change.key === 'TODOS_V2') {
       console.log('Hubo cambios en TODOS_V1');
       setStorageChange(true);
     }
